018b83ab8ea1c9014ba6f4dc8cf28a083fde6f5f
Davyson Oliveira <105729246+Davysu@users.noreply.github.com>
2024-11-21 21:00:40 -0300

    Aula 4

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,11 +1,35 @@
-import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import express from "express"; // Importa o framework Express para criar a aplicação web
+import multer from "multer"; // Importa o middleware Multer para lidar com o upload de arquivos
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js"; // Importa as funções controladoras para lidar com as requisições relacionadas a posts
 
- const routes = (app) => {
-    // Habilita o parser JSON para o Express, permitindo que o servidor entenda requisições com corpo em formato JSON.
-    app.use(express.json());
-    // Rota GET para a URL "/posts".
-    app.get("/posts",listarPosts );
-}
+// Configura o armazenamento para o Multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // Define o diretório de destino para os arquivos upados
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    // Define o nome do arquivo, mantendo o nome original
+    cb(null, file.originalname);
+  }
+});
 
-export default routes;
+// Cria uma instância do Multer com a configuração de armazenamento definida
+const upload = multer({ dest: "./uploads", storage });
+
+// Define as rotas da aplicação
+const routes = (app) => {
+  // Habilita o parser JSON para o Express, permitindo que o servidor entenda requisições com corpo em formato JSON
+  app.use(express.json());
+
+  // Rota GET para listar todos os posts
+  app.get("/posts", listarPosts);
+
+  // Rota POST para criar um novo post
+  app.post("/posts", postarNovoPost);
+
+  // Rota POST para fazer upload de uma imagem
+  app.post("/upload", upload.single("imagem"), uploadImagem);
+};
+
+export default routes; // Exporta a função de rotas para ser utilizada em outros módulos
