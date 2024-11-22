46cf8baa4d35bd80a9314d8e5b0d2bb800ba6130
Davyson Oliveira <105729246+Davysu@users.noreply.github.com>
2024-11-22 20:25:13 -0300

    Aula 5

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,20 +1,28 @@
-import conectarAoBanco from "../config/dbConfig.js";
-// Conecta ao banco de dados MongoDB usando a string de conexão fornecida como variável de ambiente.
-// A função conectarAoBanco retorna uma promessa que será resolvida com a conexão ao banco de dados.
+import 'dotenv/config';
+import { ObjectId } from "mongodb";
+import conectarAoBanco from "../config/dbConfig.js"
+// Conecta ao banco de dados utilizando a string de conexão fornecida como variável de ambiente
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-// Função assíncrona para obter todos os posts do banco de dados.
+// Função assíncrona para buscar todos os posts do banco de dados
 export async function getTodosPosts() {
-    // Obtém o banco de dados "imersao-instabytes" da conexão.
+    // Seleciona o banco de dados "imersao-instabytes"
     const db = conexao.db("imersao-instabytes");
-    // Obtém a coleção "posts" do banco de dados.
+    // Seleciona a coleção "posts" dentro do banco de dados
     const colecao = db.collection("posts");
-    // Executa uma consulta para encontrar todos os documentos na coleção e retorna os resultados como um array.
+    // Retorna um array com todos os documentos da coleção
     return colecao.find().toArray();
 }
 
 export async function criarPost(novoPost) {
-        const db = conexao.db("imersao-instabytes");
-        const colecao = db.collection("posts");
-        return colecao.insertOne(novoPost);
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost);
+}
+
+export async function atualizarPost(id, novoPost) {
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    const objID = ObjectId.createFromHexString(id);
+    return colecao.updateOne({_id: new ObjectId(objID)}, {$set:novoPost});
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,35 +1,43 @@
 import express from "express"; // Importa o framework Express para criar a aplicação web
-import multer from "multer"; // Importa o middleware Multer para lidar com o upload de arquivos
-import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js"; // Importa as funções controladoras para lidar com as requisições relacionadas a posts
+import multer from "multer"; // Importa o Multer para lidar com uploads de arquivos
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js"; // Importa as funções controladoras para lidar com a lógica dos posts
+import cors from "cors";
 
-// Configura o armazenamento para o Multer
+const corsOptions = {
+  origin: "http://localhost:8000",
+  optionsSuccessStatus: 200
+}
+
+// Configura o armazenamento do Multer para uploads de imagens
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    // Define o diretório de destino para os arquivos upados
-    cb(null, 'uploads/');
+    // Especifica o diretório para armazenar as imagens enviadas
+    cb(null, 'uploads/'); // Substitua por seu caminho de upload desejado
   },
   filename: function (req, file, cb) {
-    // Define o nome do arquivo, mantendo o nome original
-    cb(null, file.originalname);
+    // Mantém o nome original do arquivo por simplicidade
+    cb(null, file.originalname); // Considere usar uma estratégia de geração de nomes únicos para produção
   }
 });
 
-// Cria uma instância do Multer com a configuração de armazenamento definida
-const upload = multer({ dest: "./uploads", storage });
+// Cria uma instância do middleware Multer
+const upload = multer({ storage: storage });
 
-// Define as rotas da aplicação
+// Define as rotas usando o objeto Express app
 const routes = (app) => {
-  // Habilita o parser JSON para o Express, permitindo que o servidor entenda requisições com corpo em formato JSON
+  // Permite que o servidor interprete corpos de requisições no formato JSON
   app.use(express.json());
+  app.use(cors(corsOptions))
+  // Rota para recuperar uma lista de todos os posts
+  app.get("/posts", listarPosts); // Chama a função controladora apropriada
 
-  // Rota GET para listar todos os posts
-  app.get("/posts", listarPosts);
+  // Rota para criar um novo post
+  app.post("/posts", postarNovoPost); // Chama a função controladora para criação de posts
 
-  // Rota POST para criar um novo post
-  app.post("/posts", postarNovoPost);
+  // Rota para upload de imagens (assumindo uma única imagem chamada "imagem")
+  app.post("/upload", upload.single("imagem"), uploadImagem); // Chama a função controladora para processamento da imagem`
 
-  // Rota POST para fazer upload de uma imagem
-  app.post("/upload", upload.single("imagem"), uploadImagem);
+  app.put("/upload/:id", atualizarNovoPost)
 };
 
-export default routes; // Exporta a função de rotas para ser utilizada em outros módulos
+export default routes;
